4c3a7101fdff9a719824e5ab911ea69fe7c29a36
khizer ahmad <sa21aiy@herts.ac.uk>
2024-08-29 00:16:59 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+you = youDict[youstr]
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+
+# By now we have 2 numbers (variables), you and computer
+
+# We use reverseDict because it is going to convert numbers into words
+
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+if computer == you:
+    print("It's a draw!")
+
+else:
+    # if ((computer - you) == -1 or (computer - you) == 2):
+    #     print("You lose!")
+    #
+    # else:
+    #     print("You Win!")
+
+    if computer == 1 and you == -1:
+        print("You lose!")
+
+    elif computer == -1 and you == 1:
+        print("You win!")
+
+    elif computer == 1 and you == 0:
+        print("You win!")
+
+    elif computer == -1 and you == 0:
+        print("You lose!")
+
+    elif computer == 0 and you == -1:
+        print("You win!")
+
+    elif computer == 0 and you == 1:
+        print("You lose!")
+
+    else:
+        print("Something went wrong!")
